8829954eb9907ca6535bf4a2d2c942aa5386d7e8
BlueHanae <six.maxime@gmail.com>
2022-12-06 18:13:51 +0100

    dark mode 50%fini

--- js/darktheme.js
+++ js/darktheme.js
@@ -1,10 +1,17 @@
+
 function changeToDark(event){
     event.preventDefault();
-
+    const selectHeader = document.querySelector('header')
+    selectHeader.classList.toggle('dark')
     const selectBody = document.querySelector('body');
-    selectBody.classList.toggle('dark-body');
+    selectBody.classList.toggle('dark');
+    if(!selectBody.classList.contains("dark")){
+        localStorage.setItem("theme", "light-mode");
+    }else{
+        localStorage.setItem("theme", "dark-mode");
+    }
 }
-const buttonDarkTheme = document.querySelector('#dark-mode-icon');
+const buttonDarkTheme = document.querySelector('#icon-theme');
 buttonDarkTheme.addEventListener('click', changeToDark);
 
 // localStorage.setItem("theme", "dark-theme");
